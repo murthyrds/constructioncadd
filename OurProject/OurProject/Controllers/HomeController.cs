52bd3472a03a1a70fb05724ab6a3b54a43f201bb
RD-23\RD-24 <Murthygl.rds@gmail.com>
2018-05-07 18:32:58 +0530

    users display based on roles is done. users delete functionality is still pending.

--- OurProject/OurProject/Controllers/HomeController.cs
+++ OurProject/OurProject/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using OurProject.Services;
 using PagedList;
 using PagedList.Core.Mvc;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OurProject.Controllers
 {
@@ -14,19 +16,47 @@ namespace OurProject.Controllers
     {
         private IUserData _userData;
         private UserManager<User> _userManager;
+        private OurProjectDbContext _context;
 
-        public HomeController(IUserData userData, UserManager<User> userManager)
+        public HomeController(IUserData userData, UserManager<User> userManager, OurProjectDbContext context)
         {
             _userData = userData;
             _userManager = userManager;
+            _context = context;
         }
+
+        //Get the admin detail's.
         [Authorize(Roles = "Admin")]
         public IActionResult Index()
         {
             var model = new DetailsModel();
             model.users = _userData.GetAll();           
             return View(model);
-        }        
+        }
+
+        //Get the detailer's only.
+        public IActionResult Detailer()
+        {
+            var model = new DetailsModel();
+            model.users = _userData.GetDetailer();
+            return View(model);
+        }
+
+        //Get the Vendor's only.
+        public IActionResult Vendors()
+        {
+            var model = new DetailsModel();
+            model.users = _userData.GetVendor();
+            return View(model);
+        }
+
+        //Get the Vendor's only.
+        public IActionResult Client()
+        {
+            var model = new DetailsModel();
+            model.users = _userData.GetClient();
+            return View(model);
+        }
 
         public IActionResult Details(string id)
         {
@@ -71,5 +101,42 @@ namespace OurProject.Controllers
             }
             return View(model);
         }
+
+       
+        public async Task<IActionResult> Delete(string Id)
+        {
+
+            //get User Data from Userid
+            var user = await _userManager.FindByIdAsync(Id);
+
+            //List Logins associated with user
+            var logins = user;
+
+            //Gets list of Roles associated with current user
+            var rolesForUser = await _userManager.GetRolesAsync(user);
+
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+
+                if (rolesForUser.Count() > 0)
+                {
+                    foreach (var item in rolesForUser.ToList())
+                    {
+                        // item should be the name of the role
+                        var result = await _userManager.RemoveFromRoleAsync(user, item);
+                    }
+                }
+
+                //Delete User
+                await _userManager.DeleteAsync(user);
+
+                TempData["Message"] = "User Deleted Successfully. ";
+                TempData["MessageValue"] = "1";
+                //transaction.commit();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
--- OurProject/OurProject/Services/UsersData.cs
+++ OurProject/OurProject/Services/UsersData.cs
@@ -11,6 +11,9 @@ namespace OurProject.Services
     public interface IUserData
     {
         IEnumerable<User> GetAll();
+        IEnumerable<User> GetDetailer();
+        IEnumerable<User> GetClient();
+        IEnumerable<User> GetVendor();        
         User Get(string id);
         void Commit();
     }
@@ -37,11 +40,49 @@ namespace OurProject.Services
         {
             var dropdrown = new SelectList(_context.Roles.Where(u => !u.Name.Contains(""))
                                             .ToList(), "Name", "Name");
-
-            List<string> usrids = _context.UserRoles.Where(a => a.RoleId == "745e9fb4-8cfe-4338-8eb7-dd50495f5532")
+            //Admin Details
+            List<string> usrids = _context.UserRoles.Where(a => a.RoleId == "cb4417c7-c833-497c-b34d-9a561db2a706")
                 .Select(b => b.UserId).Distinct().ToList();
             List<User> listUsers = _context.Users.Where(a => usrids.Any(c => c == a.Id)).ToList();          
             return listUsers;
-        }       
+        }
+
+        public IEnumerable<User> GetDetailer()
+        {
+            var dropdrown = new SelectList(_context.Roles.Where(u => !u.Name.Contains(""))
+                                            .ToList(), "Name", "Name");
+
+            //Detailer Details.
+            List<string> usrids = _context.UserRoles.Where(a => a.RoleId == "477a59e7-7282-40e7-9b92-611a4def68f1")
+                .Select(b => b.UserId).Distinct().ToList();
+            List<User> listUsers = _context.Users.Where(a => usrids.Any(c => c == a.Id)).ToList();
+            return listUsers;
+        }
+
+        //Vendor Details
+        public IEnumerable<User> GetVendor()
+        {
+            var dropdrown = new SelectList(_context.Roles.Where(u => !u.Name.Contains(""))
+                                            .ToList(), "Name", "Name");
+
+            //Vendor Details.
+            List<string> usrids = _context.UserRoles.Where(a => a.RoleId == "385bfef5-a839-4390-95b1-2f754b77facf")
+                .Select(b => b.UserId).Distinct().ToList();
+            List<User> listUsers = _context.Users.Where(a => usrids.Any(c => c == a.Id)).ToList();
+            return listUsers;
+        }
+
+        //Client Details
+        public IEnumerable<User> GetClient()
+        {
+            var dropdrown = new SelectList(_context.Roles.Where(u => !u.Name.Contains(""))
+                                            .ToList(), "Name", "Name");
+
+            //Client Details.
+            List<string> usrids = _context.UserRoles.Where(a => a.RoleId == "745e9fb4-8cfe-4338-8eb7-dd50495f5532")
+                .Select(b => b.UserId).Distinct().ToList();
+            List<User> listUsers = _context.Users.Where(a => usrids.Any(c => c == a.Id)).ToList();
+            return listUsers;
+        }
     }
 }
